7dd9922c023a61c9e83fd8e356c78b029df7e252
DJColdBrain <creedassassin67@gmail.com>
2018-02-20 18:26:49 +0200

    Setting up spring boot

---
+++ src/main/java/djcoldbrain/appConfig.java
@@ -0,0 +1,11 @@
+package djcoldbrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+
+@EnableAutoConfiguration
+public class appConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(appConfig.class, args);
+    }
+}
